375390ca5939f64c12ef9bc86484efda2add408a
Luiz Simoes <luizgsantonio@gmail.com>
2023-10-16 19:33:19 -0300

    taskify

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -1,25 +1,23 @@
-import os # bibliotecas para manipular arquivos e pastas 
-import glob # biblioteca para listar arquivos
-
-import pandas as pd
+import glob  # biblioteca para listar arquivos
+import os  # bibliotecas para manipular arquivos e pastas
 from typing import List
 
-
-
+import pandas as pd
 
 path = 'data/input'
 
+
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
-    '''
+    """
     função para ler os arquivos excel da pasta data/input
     e retornar uma lista de dataframes
 
     args: input_path (str): data/input
 
     return: lista de dataframes
-    '''
-    all_files = glob.glob(os.path.join(path, "*.xlsx"))
-    
+    """
+    all_files = glob.glob(os.path.join(path, '*.xlsx'))
+
     data_frame_list = []
     for file in all_files:
         data_frame_list.append(pd.read_excel(file))
@@ -27,6 +25,6 @@ def extract_from_excel(path: str) -> List[pd.DataFrame]:
     return data_frame_list
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     data_frame_list = extract_from_excel(path)
     print(data_frame_list)
